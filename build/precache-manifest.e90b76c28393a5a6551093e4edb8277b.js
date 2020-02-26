95eacb8a91c3c2cc7c25bdbcb96cf987b7d9791a
Gabriel Stone <42989057+GabrielGStone@users.noreply.github.com>
2020-02-26 01:21:11 -0300

    Add files via upload

---
+++ build/precache-manifest.e90b76c28393a5a6551093e4edb8277b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f811694c7cc56d28ac7297d9281d1922",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4c87703c48ebc271fcd",
+    "url": "/static/css/main.b031b889.chunk.css"
+  },
+  {
+    "revision": "d120b5c7d9f2cbbb9d3b",
+    "url": "/static/js/2.cb454335.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.cb454335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c87703c48ebc271fcd",
+    "url": "/static/js/main.e87af4a7.chunk.js"
+  },
+  {
+    "revision": "e2d051fda34cc9d85b0b",
+    "url": "/static/js/runtime-main.d6f8d514.js"
+  }
+]);
